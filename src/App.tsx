0effc0c0e794a3ef33a77539c029a0599f2fff6b
Brennan Rouhana <brouhanaj@gmail.com>
2021-08-13 15:55:47 -0500

    add ThemeProvider to customize color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#2C698D'
+    },
+    success: {
+        main: "#219653"
+    }
+  },
+});
+
+export default theme;
