35682185f8df7829b3a79f11295000fb8d613d6a
Shaham Noorani <shahamnoorani@gmail.com>
2021-08-20 13:56:02 -0500

    updated tests

--- src/pages/Trials/TrialTabs.tsx
+++ src/pages/Trials/TrialTabs.tsx
@@ -1,86 +1,86 @@
-import React from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import TabPanel from "./TabBody/TabPanel";
 import InfoCards from "./TabBody/Cards/InfoCards";
 
 //FIXME: Replace with real data
-import data from "./dummyData.json"
+import data from "./dummyData.json";
 
 //data typescript, adapt for actual data
 interface item {
-    name: string,
-    startDate: string,
-    completionDate: string,
-    patientsCompleted: string,
-    status: string,
-    id: number
+  name: string;
+  startDate: string;
+  completionDate: string;
+  patientsCompleted: string;
+  status: string;
+  id: number;
 }
 
-let dummyData : item[] = data;
+let dummyData: item[] = data;
 
 //styling
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-    },
-    appBar: {
-        boxShadow: "none",
-        backgroundColor: "white",
-        paddingLeft: "7.4vw"
-    },
-    tabs: {
-        backgroundColor: "white"
-    }
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+  },
+  appBar: {
+    boxShadow: "none",
+    backgroundColor: "white",
+    paddingLeft: "7.4vw",
+  },
+  tabs: {
+    backgroundColor: "white",
+  },
 }));
 
 //Entry point for the Tabs
-const TrialTabs= () => {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+const TrialTabs = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    //shifts tab display
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue);
-    };
+  //shifts tab display
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
-    return (
-        <div className={classes.root}>
-            {/* Tab header */}
-            <AppBar position="static" className={classes.appBar}>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="simple tabs example"
-                    className={classes.tabs}
-                    indicatorColor="primary"
-                    textColor="primary"
-                >
-                    <Tab label="All" data-testid="all-tab" />
-                    <Tab label="Active" data-testid="active-tab" />
-                    <Tab label="Pending" data-testid="pending-tab" />
-                    <Tab label="Ending" data-testid="ending-tab" />
-                </Tabs>
-            </AppBar>
+  return (
+    <div className={classes.root}>
+      {/* Tab header */}
+      <AppBar position="static" className={classes.appBar}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+          className={classes.tabs}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="All" data-testid="all-tab" />
+          <Tab label="Active" data-testid="active-tab" />
+          <Tab label="Pending" data-testid="pending-tab" />
+          <Tab label="Ended" data-testid="ended-tab" />
+        </Tabs>
+      </AppBar>
 
-            {/* Content per tab */}
-            <TabPanel value={value} index={0}>
-              <InfoCards data={dummyData} statusShow={"all"}/>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <InfoCards data={dummyData} statusShow={"active"}/>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <InfoCards data={dummyData} statusShow={"pending"}/>
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                <InfoCards data={dummyData} statusShow={"ending"}/>
-            </TabPanel>
-        </div>
-    );
+      {/* Content per tab */}
+      <TabPanel value={value} index={0}>
+        <InfoCards data={dummyData} statusShow={"all"} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <InfoCards data={dummyData} statusShow={"active"} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <InfoCards data={dummyData} statusShow={"pending"} />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <InfoCards data={dummyData} statusShow={"ended"} />
+      </TabPanel>
+    </div>
+  );
 };
 
 export default TrialTabs;
--- src/pages/Trials/__Tests__/Trials.test.tsx
+++ src/pages/Trials/__Tests__/Trials.test.tsx
@@ -1,232 +1,179 @@
-import React from 'react'
+import React from "react";
 
-import {render, screen, fireEvent} from "@testing-library/react"
-import "@testing-library/jest-dom/extend-expect"
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import SearchBar from "../TabBody/SearchBar";
 import InfoCards from "../TabBody/Cards/InfoCards";
 import TrialTabs from "../TrialTabs";
 
+// Clicking through tabs changes status info cards status shown
+describe("clicking tabs for trial states", () => {
+  test("clicking the 'all' tab should display active trials", () => {
+    const { getByTestId, getByRole } = render(<TrialTabs />);
+    const allTab = getByTestId("all-tab");
 
-//Clicking through tabs changes status info cards status shown
-describe("tabs clicking through correctly in Trials", () => {
-    test("all tab double click", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
+    // verify that 'all' tab is the default tab on page visit
+    let selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(allTab);
 
-        let selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
+    fireEvent.click(allTab);
 
-        //double click
-        fireEvent.click(allTab);
-        fireEvent.click(allTab);
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(allTab);
+  });
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-    })
+  test("clicking the 'active' tab should display active trials", () => {
+    const { getByTestId, getByRole } = render(<TrialTabs />);
+    const activeTab = getByTestId("active-tab");
 
-    test("active tab selected on click", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
-        const activeTab = getByTestId('active-tab')
+    fireEvent.click(activeTab);
 
-        let selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(activeTab);
+    let selectedTab = getByRole("tab", { selected: true });
+    expect(activeTab).toEqual(selectedTab);
+  });
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(activeTab).toEqual(selectedTab);
+  test("clicking the 'pending' tab should display active trials", () => {
+    const { getByTestId, getByRole } = render(<TrialTabs />);
+    const pendingTab = getByTestId("pending-tab");
 
-        //double click
-        fireEvent.click(activeTab);
-        fireEvent.click(activeTab);
+    fireEvent.click(pendingTab);
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(activeTab).toEqual(selectedTab);
-    })
-    test("active tab selected on click", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
-        const activeTab = getByTestId('active-tab')
+    let selectedTab = getByRole("tab", { selected: true });
+    expect(pendingTab).toEqual(selectedTab);
+  });
 
+  test("clicking the 'ended' tab should display active trials", () => {
+    const { getByTestId, getByRole } = render(<TrialTabs />);
+    const endedTab = getByTestId("ended-tab");
 
-        let selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(activeTab);
+    fireEvent.click(endedTab);
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(activeTab).toEqual(selectedTab);
+    let selectedTab = getByRole("tab", { selected: true });
+    expect(endedTab).toEqual(selectedTab);
+  });
 
-        //double click
-        fireEvent.click(activeTab);
-        fireEvent.click(activeTab);
+  // randomize
+  test("mixture of clicks focuses on correct tab", () => {
+    const { getByTestId, getByRole } = render(<TrialTabs />);
+    const allTab = getByTestId("all-tab");
+    const activeTab = getByTestId("active-tab");
+    const pendingTab = getByTestId("pending-tab");
+    const endedTab = getByTestId("ended-tab");
+    let selectedTab;
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(activeTab).toEqual(selectedTab);
-    })
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(allTab);
+    fireEvent.click(endedTab);
 
-    test("pending tab selected on click", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
-        const pendingTab = getByTestId('pending-tab')
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(endedTab);
+    fireEvent.click(allTab);
 
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(allTab);
+    fireEvent.click(pendingTab);
 
-        let selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(pendingTab);
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(pendingTab);
+    fireEvent.click(activeTab);
 
-        selectedTab = getByRole("tab", {selected: true})
-        expect(pendingTab).toEqual(selectedTab);
+    selectedTab = getByRole("tab", { selected: true });
+    expect(selectedTab).toEqual(activeTab);
+  });
+});
 
-        //double click
-        fireEvent.click(pendingTab);
-        fireEvent.click(pendingTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(pendingTab).toEqual(selectedTab);
-    })
-
-    test("ending tab selected on click", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
-        const endingTab = getByTestId('ending-tab')
-
-
-        let selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(endingTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(endingTab).toEqual(selectedTab);
-
-        //double click
-        fireEvent.click(endingTab);
-        fireEvent.click(endingTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(endingTab).toEqual(selectedTab);
-    })
-    test("mixture of clicks focuses on correct tab", () => {
-        const {getByTestId, getByRole } = render(<TrialTabs />)
-        const allTab = getByTestId('all-tab')
-        const activeTab = getByTestId('active-tab')
-        const pendingTab = getByTestId('pending-tab')
-        const endingTab = getByTestId('ending-tab')
-        let selectedTab
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(endingTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(endingTab);
-        fireEvent.click(allTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(allTab);
-        fireEvent.click(pendingTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(pendingTab);
-        fireEvent.click(activeTab);
-
-        selectedTab = getByRole("tab", {selected: true})
-        expect(selectedTab).toEqual(activeTab);
-    })
-})
-
-
-
-//search bar successfully taking in info
+// search bar successfully taking in info
 describe("Search bar in test list", () => {
-   test(("search bar changes input"), () => {
-       const { getByTestId } = render(<SearchBar />)
-       const searchEl = getByTestId("search")
-       const searchInput = searchEl.children[0];
+  test("search bar content should change on input", () => {
+    const { getByTestId } = render(<SearchBar />);
+    const searchInput = getByTestId("search").children[0];
 
-       fireEvent.change(searchInput, {
-           target: {value: "Joe"}
-       })
+    fireEvent.change(searchInput, {
+      target: { value: "Joe" },
+    });
 
-       expect(searchInput).toHaveValue("Joe")
-   })
-})
+    expect(searchInput).toHaveValue("Joe");
+  });
+});
 
-
-
-//info cards successfully filtering through cards according to status
+// info cards successfully filtering through cards according to status
 describe("Filtering of trial info cards", () => {
-    //FIXME: Change w dummy data resembling what actually receive
-    const dummyData = [
-            {
-                "name": "Sam",
-                "startDate": "6/12/21",
-                "completionDate": "7/12/21",
-                "patientsCompleted": "0/341",
-                "status": "pending",
-                "id": 1
-            },
-            {
-                "name": "Tom",
-                "startDate": "4/30/20",
-                "completionDate": "7/10/80",
-                "patientsCompleted": "107/257",
-                "status": "active",
-                "id": 2
-            },
-            {
-                "name": "joe",
-                "startDate": "12/12/12",
-                "completionDate": "4/22/20",
-                "patientsCompleted": "0/441",
-                "status": "pending",
-                "id": 3
-            },
-            {
-                "name": "Doe",
-                "startDate": "1/1/31",
-                "completionDate": "4/16/40",
-                "patientsCompleted": "273/273",
-                "status": "ending",
-                "id": 4
-            }
-            ]
-    test("tab 'all' displays each data entry", () => {
-        const { getAllByTestId } = render(<InfoCards data={dummyData} statusShow={"all"} />)
-        const cardName = getAllByTestId("trial-name")
-            .map((li) => (li.textContent))
+  //FIXME: Change w dummy data resembling what actually receive
+  const dummyData = [
+    {
+      name: "Sam",
+      startDate: "6/12/21",
+      completionDate: "7/12/21",
+      patientsCompleted: "0/341",
+      status: "pending",
+      id: 1,
+    },
+    {
+      name: "Tom",
+      startDate: "4/30/20",
+      completionDate: "7/10/80",
+      patientsCompleted: "107/257",
+      status: "active",
+      id: 2,
+    },
+    {
+      name: "joe",
+      startDate: "12/12/12",
+      completionDate: "4/22/20",
+      patientsCompleted: "0/441",
+      status: "pending",
+      id: 3,
+    },
+    {
+      name: "Doe",
+      startDate: "1/1/31",
+      completionDate: "4/16/40",
+      patientsCompleted: "273/273",
+      status: "ended",
+      id: 4,
+    },
+  ];
+  test("tab 'all' displays each data entry", () => {
+    const { getAllByTestId } = render(
+      <InfoCards data={dummyData} statusShow={"all"} />
+    );
+    const cardName = getAllByTestId("trial-name").map(li => li.textContent);
 
-        const actualName = dummyData.map((x) => x.name)
+    const actualName = dummyData.map(x => x.name);
 
-        expect(cardName).toEqual(actualName);
-    })
+    expect(cardName).toEqual(actualName);
+  });
 
-    test("tab 'active' displays 'active' data entry", () => {
-        const { getAllByTestId } = render(<InfoCards data={dummyData} statusShow={"active"} />)
+  test("tab 'active' displays 'active' data entry", () => {
+    const { getAllByTestId } = render(
+      <InfoCards data={dummyData} statusShow={"active"} />
+    );
 
-        const cardStatus = getAllByTestId("trial-status")
-            .map(li => li.textContent)
+    const cardStatus = getAllByTestId("trial-status").map(li => li.textContent);
 
-        const areAllActive = cardStatus.every((status) => status === "active");
-        expect(areAllActive).toBe(true);
-    })
+    const areAllActive = cardStatus.every(status => status === "active");
+    expect(areAllActive).toBe(true);
+  });
 
-    test("tab 'pending' displays 'pending' data entry", () => {
-        const { getAllByTestId } = render(<InfoCards data={dummyData} statusShow={"pending"} />)
+  test("tab 'pending' displays 'pending' data entry", () => {
+    const { getAllByTestId } = render(
+      <InfoCards data={dummyData} statusShow={"pending"} />
+    );
 
-        const cardStatus = getAllByTestId("trial-status")
-            .map(li => li.textContent)
+    const cardStatus = getAllByTestId("trial-status").map(li => li.textContent);
 
-        const areAllPending = cardStatus.every((status) => status === "pending");
-        expect(areAllPending).toBe(true);
-    })
+    const areAllPending = cardStatus.every(status => status === "pending");
+    expect(areAllPending).toBe(true);
+  });
 
-    test("tab 'ending' displays 'ending' data entry", () => {
-        const { getAllByTestId } = render(<InfoCards data={dummyData} statusShow={"ending"} />)
+  test("tab 'ended' displays 'ended' data entry", () => {
+    const { getAllByTestId } = render(
+      <InfoCards data={dummyData} statusShow={"ended"} />
+    );
 
-        const cardStatus = getAllByTestId("trial-status")
-            .map(li => li.textContent)
+    const cardStatus = getAllByTestId("trial-status").map(li => li.textContent);
 
-        const areAllEnding = cardStatus.every((status) => status === "ending");
-        expect(areAllEnding).toBe(true);
-    })
-})
+    const areAllEnding = cardStatus.every(status => status === "ended");
+    expect(areAllEnding).toBe(true);
+  });
+});
