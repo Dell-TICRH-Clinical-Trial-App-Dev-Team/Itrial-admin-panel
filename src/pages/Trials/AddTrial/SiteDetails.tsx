0e9a4aeae3353371da36181339abce4bad831637
Shaham Noorani <shahamnoorani@gmail.com>
2021-09-20 09:00:30 -0500

    small

--- src/pages/Trials/AddTrial/SiteDetails.tsx
+++ src/pages/Trials/AddTrial/SiteDetails.tsx
@@ -61,7 +61,7 @@ export default function SiteDetails() {
               </div>
             </div>
           </div>
-          <TeamMemberDetails />
+          {/* <TeamMemberDetails /> */}
         </>
       ))}
     </div>
